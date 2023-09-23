dae44b8765ea7b1ff85a75ae999193ad2028aba2
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-23 13:30:09 +0530

    feat: navbar util

---
+++ src/utils/navbar.js
@@ -0,0 +1,15 @@
+export const navbar = [
+  { name: "HOME", id: "home" },
+  { name: "ABOUT", id: "about" },
+  {
+    name: "OUR PRODUCTS",
+    id: "product",
+    child: [
+      { name: "PRODUCT 1", id: "p1" },
+      { name: "PRODUCT 2", id: "p2" },
+      { name: "PRODUCT 3", id: "p3" },
+      { name: "PRODUCT 4", id: "p4" },
+    ],
+  },
+  { name: "CONTACT US", id: "contact" },
+];
