41326fd30c4bb042a5930c72abb470fbd1f36037
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-23 13:28:52 +0530

    feat: home screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import UpperNavbar from "./components/UpperNavbar/UpperNavbar";
+import "../src/components/UpperNavbar/UpperNavbar.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import "./components/Navbar/Navbar.css";
+import "./components/Home/Home.css";
 
 function App() {
-  return <>MY APP</>;
+  return (
+    <Home>
+      <UpperNavbar />
+      <Navbar />
+    </Home>
+  );
 }
 
 export default App;
