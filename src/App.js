559bba3f62c11fbf766d311c3e75cc9dd15a23f9
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-24 22:41:54 +0530

    feat: product & footer added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import "./App.css";
 import UpperNavbar from "./components/UpperNavbar/UpperNavbar";
-import "../src/components/UpperNavbar/UpperNavbar.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import ProductListing from "./components/ProductListing/ProductListing";
+import Footer from "./components/Footer/Footer";
+import "../src/components/UpperNavbar/UpperNavbar.css";
 import "./components/Navbar/Navbar.css";
 import "./components/Home/Home.css";
+import "./components/ProductCard/ProductCard.css";
+import "./components/ProductListing/ProductListing.css";
+import "./components/Footer/Footer.css";
 
 function App() {
   return (
-    <Home>
-      <UpperNavbar />
-      <Navbar />
-    </Home>
+    <>
+      <Home>
+        <UpperNavbar />
+        <Navbar />
+      </Home>
+      <ProductListing />
+      <Footer />
+    </>
   );
 }
 
